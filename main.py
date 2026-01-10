d90f3ed76c45d42c8cd7ddd2a3f6eab843b9c401
seneshka bimsara <142817734+seneshka99@users.noreply.github.com>
2026-01-10 18:52:28 +0530

    Improve item calculation and PDF writing methods
    fix the calculation method and add campany name,mobile number and finaly calculate the   bill of last price

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 class pos:
     def __init__(self):
          self.curent_items = []
-         self.items = []
+         self.sumArry = []
          
     def LOdedata(self):
         with open("./data/data.json") as file:
@@ -11,12 +11,12 @@ class pos:
         self.product = data.get("products",[])
         print("database lode sucss")
     def calculate(self,barcode,unit):
-        self.sum = 0
         for item in self.product:
             if item.get("barcode") == barcode:
                 price_per = item.get("price_per_unit") #calculate the sum and total price 
                 total = price_per * unit
-                self.sum +=total
+               # self.sum +=total
+                self.sumArry.append(total)
                 self.curent_items = []                     #add the list for value
                 self.curent_items.append(item.get("name")) #0
                 self.curent_items.append(total) #1
@@ -26,11 +26,12 @@ class pos:
                 break
             
     #this function is use to create text fils
-    def make_pd(self,number):
-        filename = f"./pdf/bill{number}.txt" #add you're file or file path you wont to should acreate a bll
+    def make_pdf(self,number):
+        filename = f"./pdf/bill{number}.txt"
         self.file = open(filename,"x")
-    def make_pdf(self):
-        if len(self.curent_items)<6:
+        
+    def wrirePdf(self,camand,lastPrice):
+        if lastPrice is None and camand== "no":
             data_dict = { #add the curent_items for values
                      "Name": self.curent_items[0],
                       "Total": self.curent_items[1],
@@ -45,18 +46,21 @@ class pos:
             self.file.write(f"Unit Name =  {data_dict.get('Unit Name')}\n")
             self.file.write(f"Expiry Date =  {data_dict.get('Expiry Date')}\n")
         else:
-            self.file.write(f"all price =  {self.curent_items[-1]}") 
+            self.file.write(f"all price = {lastPrice}\n")
+            self.file.write("Mobile : 071336405") 
             
     
 
 m1  = pos()
 m1.LOdedata()
 number =1
-m1.make_pd(number)
+m1.make_pdf(number)
 while True:
     # print("-----se super market----")
+    m1.file.write("----SE SOFTWARE CREATION----\n\n\n")
     key = input("Checkout now? (yes/no): ").lower()
     if key != "yes":
+        comand = "no"
         barcode = input("enter the barcode number:")
         while True:
             try:#chack the usint number is integer
@@ -66,12 +70,16 @@ while True:
                 print("-----data type in not valid----")
             
         m1.calculate(barcode,unit)
-        m1.make_pdf()
+        m1.wrirePdf(comand,None) #222
 
     else:
-        m1.curent_items.append(m1.sum)
+        m1.curent_items.clear() #clean the creant items arry
+        sum = 0
+        for cal in m1.sumArry:
+            sum+=cal
+        m1.wrirePdf("yes",sum)
+        m1.sumArry.clear() #clean the arry
         number +=1
-        m1.make_pd(number)
+        m1.make_pdf(number)
         
         
-
